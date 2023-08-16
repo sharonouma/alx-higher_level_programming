b0ee2fd7614723c57f980d00c6e715213781ba8d
Your Name <you@example.com>
2023-08-16 15:14:22 +0000

    commit

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_set = set()
+    for i in set_1:
+        if i not in set_2:
+            new_set.add(i)
+    for j in set_2:
+        if j not in set_1:
+            new_set.add(j)
+    return new_set
