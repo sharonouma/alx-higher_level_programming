0ab9012e4eb1167c9684ba4da83ac7a42e2eae67
Your Name <you@example.com>
2023-08-30 20:57:18 +0000

    commit

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,58 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class MagicClass"""
 import math
 
 
-"""
-MagicClass class definition
-"""
-
-
 class MagicClass:
-    """
-    A class that represents a magic circle with a given radius.
-
-    Attributes:
-        __radius (float): The radius of the magic circle.
-
-    Methods:
-        __init__(self, radius=0): Initializes a new instance of the
-                                MagicClass.
-        area(self): Calculates and returns the area of the magic circle.
-        circumference(self): Calculates and returns the circumference
-                            of the magic circle.
-    """
-
+    """This represents a circle"""
     def __init__(self, radius=0):
-        """
-        Initializes a new instance of the MagicClass.
-
-        Args:
-            radius (float): The radius of the magic circle.
-
-        Raises:
-            TypeError: If the radius is not a number (int or float).
-        """
+        """Initializes the Magic Class"""
         self.__radius = 0
-
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-
         self.__radius = radius
 
     def area(self):
-        """
-        Calculates and returns the area of the magic circle.
-
-        Returns:
-            float: The area of the magic circle.
-        """
-        return math.pi * self.__radius ** 2
+        """Calculaes the area of the circle"""
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """
-        Calculates and returns the circumference of the magic circle.
-
-        Returns:
-            float: The circumference of the magic circle.
-        """
+        """Calculates the circumference of the circle"""
         return 2 * math.pi * self.__radius
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""Define a class square."""
+"""Defines a class Square"""
 
 
 class Square:
-    """Represent a square."""
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        """
+        """initializes the square
         Args:
-            size (int): The size of the new square.
+            size (int): size of a side of the square
+        Returns:
+            None
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
 
     def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
         """
-        Calculates and returns the area of the square.
-        """
-        return self.__size *
+        return (self.__size) ** 2
